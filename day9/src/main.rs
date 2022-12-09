f9682a59b1186e8cc727a6532bed96dbcb68f2ed
Daniel Kimbel <dkimbel@ginkgobioworks.com>
2022-12-08 23:05:37 -0700

    Solve day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,6 +7,11 @@ fn main() {
     rope_simulator.simulate_from_file("resources/input_1");
     let solution_1 = rope_simulator.num_spaces_tail_visited();
     println!("Part 1 solution: {}", solution_1);
+
+    let mut longer_simulator = RopeSimulator::new(10);
+    longer_simulator.simulate_from_file("resources/input_1");
+    let solution_2 = longer_simulator.num_spaces_tail_visited();
+    println!("Part 2 solution: {}", solution_2);
 }
 
 #[derive(Hash, Clone, Copy, PartialEq, Eq, Debug)]
@@ -93,7 +98,6 @@ impl RopeSimulator {
                 let new_knot_loc = if i == 0 {
                     RopeSimulator::new_head_loc(*knot_loc, movement.direction)
                 } else {
-                    let relative_head_loc = maybe_previous_knot_loc.unwrap();
                     RopeSimulator::new_tail_loc(maybe_previous_knot_loc.unwrap(), *knot_loc)
                 };
                 maybe_previous_knot_loc = Some(new_knot_loc);
@@ -130,13 +134,34 @@ impl RopeSimulator {
         let x_diff = head_loc.x - tail_loc.x;
         let y_diff = head_loc.y - tail_loc.y;
 
-        if x_diff.abs() > 2 || y_diff.abs() > 2 || (x_diff.abs() + y_diff.abs() > 3) {
+        if x_diff.abs() > 2 || y_diff.abs() > 2 {
             panic!(
                 "Illegal rope position! Head {:?} and tail {:?} are too far apart",
                 head_loc, tail_loc
             )
         }
-        if x_diff == -2 {
+        // there's got to be a cleaner way to do this
+        if x_diff == 2 && y_diff == 2 {
+            Coords {
+                x: tail_loc.x + 1,
+                y: tail_loc.y + 1,
+            }
+        } else if x_diff == 2 && y_diff == -2 {
+            Coords {
+                x: tail_loc.x + 1,
+                y: tail_loc.y - 1,
+            }
+        } else if x_diff == -2 && y_diff == -2 {
+            Coords {
+                x: tail_loc.x - 1,
+                y: tail_loc.y - 1,
+            }
+        } else if x_diff == -2 && y_diff == 2 {
+            Coords {
+                x: tail_loc.x - 1,
+                y: tail_loc.y + 1,
+            }
+        } else if x_diff == -2 {
             Coords {
                 x: tail_loc.x - 1,
                 y: head_loc.y, // automatically deal with diagonal
