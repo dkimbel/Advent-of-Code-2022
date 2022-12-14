5b8a80ef4f13169f98bba20eda048c19677e61fa
Daniel Kimbel <dkimbel@ginkgobioworks.com>
2022-12-13 22:47:37 -0700

    First crack at solution, but doesn't work

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -16,6 +16,8 @@ struct Coords {
 }
 
 impl Coords {
+    const INITIAL_SAND: Self = Self { x: 500, y: 0 };
+
     fn new(s: &str) -> Self {
         let (x, y) = s.split_once(',').unwrap();
         Self {
@@ -23,15 +25,57 @@ impl Coords {
             y: y.parse::<u32>().unwrap(),
         }
     }
+
+    fn after_sand_move(&self) -> (Self, Self, Self) {
+        let first_choice = Self {
+            x: self.x,
+            y: self.y + 1,
+        };
+        let second_choice = Self {
+            x: self.x - 1,
+            y: self.y + 1,
+        };
+        let third_choice = Self {
+            x: self.x + 1,
+            y: self.y + 1,
+        };
+        (first_choice, second_choice, third_choice)
+    }
 }
 
 struct SandSimulator {
     rock_coords: HashSet<Coords>,
+    max_y: u32,
 }
 
 impl SandSimulator {
-    fn simulate(&self) -> u32 {
-        todo!()
+    fn simulate(&self) -> usize {
+        let mut all_resting_sand_coords = HashSet::new();
+        let mut sand_reached_void = false;
+
+        while !sand_reached_void {
+            let mut sand_unit_coords = Coords::INITIAL_SAND;
+            loop {
+                let mut moved = false;
+                let (dest0, dest1, dest2) = sand_unit_coords.after_sand_move();
+                for dest in [dest0, dest1, dest2] {
+                    if !self.rock_coords.contains(&dest) && !all_resting_sand_coords.contains(&dest)
+                    {
+                        sand_unit_coords = dest;
+                        moved = true;
+                        break;
+                    }
+                }
+                if !moved {
+                    all_resting_sand_coords.insert(sand_unit_coords);
+                    break;
+                } else if sand_unit_coords.y > self.max_y {
+                    sand_reached_void = true;
+                }
+            }
+        }
+
+        all_resting_sand_coords.len()
     }
 
     fn new(file_path: &str) -> Self {
@@ -39,6 +83,7 @@ impl SandSimulator {
         let reader = BufReader::new(file);
 
         let mut rock_coords = HashSet::new();
+        let mut overall_max_y = 0;
 
         for line in reader.lines() {
             let line_content = line.unwrap();
@@ -57,11 +102,13 @@ impl SandSimulator {
 
                     for y in min_y..=max_y {
                         rock_coords.insert(Coords { x, y });
+                        overall_max_y = cmp::max(overall_max_y, y);
                     }
                 } else if coords_left.y == coords_right.y {
                     let min_x = cmp::min(coords_left.x, coords_right.x);
                     let max_x = cmp::max(coords_left.x, coords_right.x);
                     let y = coords_left.y;
+                    overall_max_y = cmp::max(overall_max_y, y);
 
                     for x in min_x..=max_x {
                         rock_coords.insert(Coords { x, y });
@@ -75,6 +122,9 @@ impl SandSimulator {
             }
         }
 
-        Self { rock_coords }
+        Self {
+            rock_coords,
+            max_y: overall_max_y,
+        }
     }
 }
