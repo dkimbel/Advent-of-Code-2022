bd064927e1c4a3bc11444ee43c41191e3118561a
Daniel Kimbel <dkimbel@ginkgobioworks.com>
2022-12-18 18:18:07 -0700

    Refactor part 1 solution

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -3,20 +3,22 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let file = File::open("resources/input_1").unwrap();
-    let reader = BufReader::new(file);
+    let coords = parse_coords("resources/input_1");
+    let (full_surface_area, occupied_coords) = solve_part_one(&coords);
+    println!("Part 1 solution: {}", full_surface_area);
+    solve_part_two(occupied_coords);
+}
 
-    let mut occupied_coords = HashSet::new();
+fn solve_part_two(occupied_coords: HashSet<(isize, isize, isize)>) -> isize {
+    todo!()
+}
+
+fn solve_part_one(coords: &[(isize, isize, isize)]) -> (isize, HashSet<(isize, isize, isize)>) {
+    let mut occupied_coords: HashSet<(isize, isize, isize)> = HashSet::new();
     let mut surface_area = 0;
 
-    for line in reader.lines() {
-        let line_content = line.unwrap();
-        let split = line_content
-            .splitn(3, ',')
-            .map(|s| s.parse::<isize>().unwrap())
-            .collect::<Vec<_>>();
-        let (x, y, z) = (split[0], split[1], split[2]);
-
+    for triplet in coords {
+        let (x, y, z) = (triplet.0, triplet.1, triplet.2);
         let num_occupied_adjacents = occupied_coords
             .intersection(&HashSet::from([
                 (x + 1, y, z),
@@ -26,13 +28,30 @@ fn main() {
                 (x, y, z + 1),
                 (x, y, z - 1),
             ]))
-            .collect::<Vec<_>>()
-            .len();
+            .count();
         let surface_area_diff = 6 - (num_occupied_adjacents as isize * 2);
 
         surface_area += surface_area_diff;
         occupied_coords.insert((x, y, z));
     }
 
-    println!("Part 1 solution: {}", surface_area);
+    (surface_area, occupied_coords)
+}
+
+fn parse_coords(file_path: &str) -> Vec<(isize, isize, isize)> {
+    let file = File::open(file_path).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut parsed_coords = Vec::new();
+
+    for line in reader.lines() {
+        let line_content = line.unwrap();
+        let split = line_content
+            .splitn(3, ',')
+            .map(|s| s.parse::<isize>().unwrap())
+            .collect::<Vec<_>>();
+        parsed_coords.push((split[0], split[1], split[2]));
+    }
+
+    parsed_coords
 }
