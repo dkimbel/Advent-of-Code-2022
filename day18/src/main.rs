087740a2290e8aac45807ce7bf1d28cd26a85639
Daniel Kimbel <dkimbel@ginkgobioworks.com>
2022-12-18 18:40:17 -0700

    Sketch out approach for day 18 part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,4 +1,5 @@
-use std::collections::HashSet;
+use std::cmp;
+use std::collections::{HashSet, VecDeque};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -6,15 +7,67 @@ fn main() {
     let coords = parse_coords("resources/input_1");
     let (full_surface_area, occupied_coords) = solve_part_one(&coords);
     println!("Part 1 solution: {}", full_surface_area);
-    solve_part_two(occupied_coords);
+    let outer_surface_area = solve_part_two(occupied_coords);
+    println!("Part 2 solution: {}", outer_surface_area);
 }
 
-fn solve_part_two(occupied_coords: HashSet<(isize, isize, isize)>) -> isize {
-    todo!()
+type Coords = (isize, isize, isize);
+
+// Check surface area from the outside, by effectively sending a single-coords-occuping
+// 'particle' around the full outside surface. To do this (possibly naively?), search until
+// the particle has visited every space in the cube that it possibly can.
+fn solve_part_two(occupied_coords: HashSet<Coords>) -> usize {
+    let mut coords_discovered_from_neg_x: HashSet<Coords> = HashSet::new();
+    let mut coords_discovered_from_pos_x: HashSet<Coords> = HashSet::new();
+    let mut coords_discovered_from_neg_y: HashSet<Coords> = HashSet::new();
+    let mut coords_discovered_from_pos_y: HashSet<Coords> = HashSet::new();
+    let mut coords_discovered_from_neg_z: HashSet<Coords> = HashSet::new();
+    let mut coords_discovered_from_pos_z: HashSet<Coords> = HashSet::new();
+
+    let mut min_x = isize::MIN;
+    let mut max_x = isize::MAX;
+    let mut min_y = isize::MIN;
+    let mut max_y = isize::MAX;
+    let mut min_z = isize::MIN;
+    let mut max_z = isize::MAX;
+
+    for coords in &occupied_coords {
+        let (x, y, z) = (coords.0, coords.1, coords.2);
+        min_x = cmp::min(min_x, x);
+        max_x = cmp::max(max_x, x);
+        min_y = cmp::min(min_y, y);
+        max_y = cmp::max(max_y, y);
+        min_z = cmp::min(min_z, z);
+        max_z = cmp::max(max_z, z);
+    }
+
+    let starting_search_coords = (min_x, min_y, min_z);
+    if occupied_coords.contains(&starting_search_coords) {
+        panic!("Attempted to start search from occupied coordinates!");
+    }
+
+    let mut visited: HashSet<Coords> = HashSet::new();
+    let mut coords_to_search = VecDeque::from([starting_search_coords]);
+
+    while let Some(coords) = coords_to_search.pop_front() {
+        // todo no-op if visited
+        // todo no-op if out of bounds
+        // todo check surrounding six tiles
+        //   todo if occupied by lava, mutate relevant hashmap
+        //   todo if unoccupied by lava, push to coords_to_search
+        // todo add curr to visited
+    }
+
+    coords_discovered_from_neg_x.len()
+        + coords_discovered_from_pos_x.len()
+        + coords_discovered_from_neg_y.len()
+        + coords_discovered_from_pos_y.len()
+        + coords_discovered_from_neg_z.len()
+        + coords_discovered_from_pos_z.len()
 }
 
-fn solve_part_one(coords: &[(isize, isize, isize)]) -> (isize, HashSet<(isize, isize, isize)>) {
-    let mut occupied_coords: HashSet<(isize, isize, isize)> = HashSet::new();
+fn solve_part_one(coords: &[Coords]) -> (isize, HashSet<Coords>) {
+    let mut occupied_coords: HashSet<Coords> = HashSet::new();
     let mut surface_area = 0;
 
     for triplet in coords {
@@ -38,7 +91,7 @@ fn solve_part_one(coords: &[(isize, isize, isize)]) -> (isize, HashSet<(isize, i
     (surface_area, occupied_coords)
 }
 
-fn parse_coords(file_path: &str) -> Vec<(isize, isize, isize)> {
+fn parse_coords(file_path: &str) -> Vec<Coords> {
     let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
 
